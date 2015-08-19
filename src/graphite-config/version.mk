fabbb83a0f2486adec4b54292d0db251156d37dd
root <root@vi-1.rocksclusters.org>
2015-08-19 11:24:15 -0700

    basic rpm builds didn't include configuration examples.

--- src/graphite-config/version.mk
+++ src/graphite-config/version.mk
@@ -1,3 +1,10 @@
 NAME		= graphite-config
 RELEASE		= 0
-RPM.PREFIX	= /etc/init.d
+VERSION		= 0.9.9
+GRAPHITE_VERSION = $(VERSION)
+CARBON_VERSION = $(GRAPHITE_VERSION)
+TAR_POSTFIX	= tar.gz
+GRAPHITE = graphite-web
+CARBON =   carbon
+#RPM.PREFIX	= /etc/init.d
+
